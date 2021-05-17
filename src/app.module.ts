1fc7156387fc2a87ad4ea164b5bd42f0c6b9b23e
Gustavo Aldar <gustavoaldar@outlook.com>
2021-05-17 14:48:53 -0300

    created env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
+import 'dotenv/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://gustavo:CEV5nps8fVhEZ7PM@mongodb.zenp9.mongodb.net/kuanto_kusta?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-      },
-    ),
+    MongooseModule.forRoot(process.env.MONGO_DB, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }),
     ProductModule,
   ],
   controllers: [],
